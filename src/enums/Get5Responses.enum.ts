aab299e68a989316db8423eb533326683bbd7ba8
kddige <emildige@gmail.com>
2021-10-29 15:28:23 +0200

    Fixed error responses

--- src/enums/Get5Responses.enum.ts
+++ src/enums/Get5Responses.enum.ts
@@ -6,8 +6,8 @@ export enum Get5Responses {
   ERROR_CANNOT_LOAD_URL_MISSING_STEAMWORKS = 'Cannot load matches from a url without the SteamWorks extension running',
   
   // Success
-  SUCCESS_LOADED_MATCH_CONFIG = "[ Get5 ] Loaded match config.",
-  SUCCESS_MATCH_ENDED = "[ Get5 ] An admin force ended the match.",
+  SUCCESS_LOADED_MATCH_CONFIG = "Loaded match config.",
+  SUCCESS_MATCH_ENDED = "An admin force ended the match.",
   
 
   // Default
